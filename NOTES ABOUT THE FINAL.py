42e39250c8c80fe35c61fd42ca28b8a0a78040d6
Kassidy-A <kassidyvictoria03@gmail.com>
2025-04-27 22:39:17 -0500

    Add files via upload

---
+++ NOTES ABOUT THE FINAL.py
@@ -0,0 +1,75 @@
+
+#GROUP PROJECT
+
+# Latex Project
+######WIth title, date, members, an intro
+
+###############################################################################
+
+# Set up Environment
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+###############################################################################
+
+# Load and Clean the Data
+
+df = pd.read_csv()
+
+
+df.isnull().sum()      #checks for missing values
+df.dropna(inplace=True)     #drops missing values
+df.duplicated().sum       #checks for duplicate data
+df.drop_duplicates(inplace=True)  
+
+###############################################################################
+
+# Summary Stats, Descriptive Stats, and Correlation
+
+#one line of code 
+#df.describe 
+#what do these stats tell you
+
+###############################################################################
+
+# Analysis ???????????????????????? 
+
+#literally said it isn't a specific thing????????
+#depends on you and your group
+#what info you want to show from your group
+
+
+#stats one column, depending on the data and what it means???????
+
+###############################################################################
+
+# Visualization:
+
+    #Choose 1 or 2??????
+#Histogram
+#Boxplot
+#Bar Chart
+
+###############################################################################
+
+# Linear Regression Model
+
+#line of best fit
+
+###############################################################################
+
+# Other Analysis Ideas:
+
+    
+
+
+
+
+
+
+
+
+
+
